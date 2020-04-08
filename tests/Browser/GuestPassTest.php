12a7249240d7f15a40544642c0b096d18f5fb0c7
Jose Pacheco <jose@icheko.com>
2020-04-08 00:18:56 -0700

    only run registerGuestPass
    Signed-off-by: Jose Pacheco <jose@icheko.com>

--- tests/Browser/GuestPassTest.php
+++ tests/Browser/GuestPassTest.php
@@ -28,7 +28,6 @@ class GuestPassTest extends DuskTestCase
     /**
      * @throws \Throwable
      * @test
-     * @group ignore
      */
     public function registerGuestPass(){
         $contact = new PersonContact();
@@ -44,65 +43,5 @@ class GuestPassTest extends DuskTestCase
         $vehicle->color = 'Gray';
 
         $this->register('14905 W Navarre Way', $contact, $vehicle);
-    }
-
-    /**
-     * @return void
-     * @throws \Throwable
-     */
-    public function testVerifyData()
-    {
-        $this->browse(function (Browser $browser) {
-            $this->verifyStep1($browser);
-        });
-    }
-
-    /**
-     * @param $browser
-     */
-    public function verifyStep1($browser){
-
-        $this->addMacros();
-
-        $browser->visit('/')
-            ->click('a[href$="TEMPORARYPERMIT"]')
-            ->assertSee('Step 1 - Your Location')
-            ->type('ctl00$ContentPlaceHolder1$Wizard1$WucUnitSelector1$stxtPropertyName', 'cabrillo')
-            ->press('ctl00$ContentPlaceHolder1$Wizard1$WucUnitSelector1$scmdFind')
-            ->waitForText('Choose Property')
-            ->assertSelectHasOptions('ctl00$ContentPlaceHolder1$Wizard1$WucUnitSelector1$scboPropertyIDFK', ['62399'])
-            ->select('ctl00$ContentPlaceHolder1$Wizard1$WucUnitSelector1$scboPropertyIDFK', '62399')
-            ->waitForText('Choose Address')
-            ->assertSelectHasOptions('ctl00$ContentPlaceHolder1$Wizard1$WucUnitSelector1$scboPropertyAddressIDFK', array_keys($this->address_ids))
-            ->select('ctl00$ContentPlaceHolder1$Wizard1$WucUnitSelector1$scboPropertyAddressIDFK', array_search('12347 N Aragon Way', $this->address_ids))
-            ->waitForText('Choose Unit Number')
-            ->selectSecondOption('ctl00$ContentPlaceHolder1$Wizard1$WucUnitSelector1$scboUnitIDFK')
-            ->press('ctl00$ContentPlaceHolder1$Wizard1$StartNavigationTemplateContainerID$StartNextButton')
-            ->assertSee('If you do not fill out your email address');
-    }
-
-    /**
-     * @return void
-     * @throws \Throwable
-     */
-    public function testParkingPermitRegistration()
-    {
-        $contact = new PersonContact();
-        $contact->first_name = 'Jose';
-        $contact->last_name = 'Pacheco';
-        $contact->email = 'icheko@gmail.com';
-        $contact->phone = '8186758615';
-
-        try {
-            $this->register('12347 N Aragon Way', $contact, new GuestVehicle);
-        } catch (\Exception $e) {
-            $expectedMessage = 'Your information does not match what we have on file. Please try again.';
-            $error = $e->getMessage();
-            PHPUnit::assertTrue(
-                Str::contains($error, $expectedMessage),
-                "Expected exception [\"{$expectedMessage}\"] received [\"{$error}\"]."
-            );
-        }
-
     }
 }
